21e50699008fbad447595036e251af2c6512e17b
Akshat Kumar Tambe <akshat.jec@gmail.com>
2020-06-26 23:42:45 +0200

    almost done

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -10,34 +10,80 @@ GAME RULES:
 */
 
 
-
-
 var scores, roundScores, activePlayer, dice;
 
-scores = [0,0];
+scores = [0, 0];
 roundScores = 0;
 activePlayer = 0;
 
-// Add Event listener for 'New Game' button.
-document.querySelector('.btn-new').addEventListener('click', function () {
-// Reset Initial values to 0
+function resetGameOnPageLoad() {
+    // Reset Initial values to 0
     document.getElementById('score-0').textContent = '0';
-    document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
+
+    document.getElementById('score-1').textContent = '0';
     document.getElementById('current-1').textContent = '0';
 
 // Reset Dice to none i.e. do not show dice in the beginning.
     document.querySelector('.dice').style.display = 'none';
-});
+    roundScores = 0;
+    activePlayer = 0;
+    scores = [0, 0];
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+}
+
+function resetRounderScoreAndActivePlayer() {
+    roundScores = 0;
+    document.querySelector('#current-' + activePlayer).textContent = roundScores;
+
+    // 3. Change the active user.
+    document.querySelector('.player-'+ activePlayer +'-panel').classList.toggle('active');
+    (activePlayer === 0) ? activePlayer = 1 : activePlayer = 0;
+    document.querySelector('.player-'+ activePlayer +'-panel').classList.toggle('active');
+}
+
+// Reset game on Page load.
+resetGameOnPageLoad();
+
+
+// Add Event listener for 'New Game' button. Reset values on click.
+document.querySelector('.btn-new').addEventListener('click', resetGameOnPageLoad);
 
 // Add event listener for 'Roll Dice' button.
-document.querySelector('.btn-roll').addEventListener('click', function (){
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    // 1. Random number
     let dice = Math.floor(Math.random() * 6) + 1;
+    console.log(dice);
+
+    // 2. Display the dice
     let x = document.querySelector('.dice');
     x.style.display = 'block';
     x.src = `dice-${dice}.png`;
+
+    // 3. Add numbers in the current box until Dice rolls to 1
+    if(dice === 1){
+        resetRounderScoreAndActivePlayer();
+    }else{
+        roundScores += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScores;
+    }
 });
 
+// Add event Listener for 'Hold' button
+document.querySelector('.btn-hold').addEventListener('click', function () {
+    // 1. Added rounded score to Scores array w.r.t activePlayer.
+   scores[activePlayer] += roundScores;
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    // 2. Reset rounded score to 0.
+    resetRounderScoreAndActivePlayer();
+
+    // Calculate for first one to reach 100 points.
+    if(scores[activePlayer] >= 100){
+
+    }
+})
 
 
 
